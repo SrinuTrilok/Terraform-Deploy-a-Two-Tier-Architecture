b62d058b16ac8885bddea454d9940abfd650e6c8
SrinuTrilok <120594073+SrinuTrilok@users.noreply.github.com>
2024-04-18 23:29:23 +0530

    Update vpc-subnet.tf

--- vpc-subnet.tf
+++ vpc-subnet.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "TF-Demo" {
 resource "aws_subnet" "public_subnetTF-Demo" {
   vpc_id            = aws_vpc.TF-Demo.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1"
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "public subnet TF-Demo"
   }
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnetTF-Demo" {
 resource "aws_subnet" "public_subnetTF-Demo" {
   vpc_id            = aws_vpc.TF-Demo.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1"
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "public subnet TF-Demo"
   }
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_subnetTF-Demo" {
 resource "aws_subnet" "private_subnetTF-Demo" {
   vpc_id                  = aws_vpc.TF-Demo.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "ap-south-1"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
     Name = "private subnet TF-Demo"
@@ -39,7 +39,7 @@ resource "aws_subnet" "private_subnetTF-Demo" {
 resource "aws_subnet" "private_subnetTF-Demo" {
   vpc_id                  = aws_vpc.TF-Demo.id
   cidr_block              = "10.0.4.0/24"
-  availability_zone       = "ap-south-1"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
     Name = "private subnet TF-Demo"
